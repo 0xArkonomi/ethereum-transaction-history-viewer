47873f861fc97a6822b62717ea8465b2f2a4753c
Mohammad Mahdi Keshavarz <arkonomii@gmail.com>
2023-09-30 15:45:13 +0330

    Add getCountractData() function

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ async function connectToEthereum() {
         console.error('MetaMask is not installed.');
     }
 }
+
+async function getContractData() {
+    const ethAddress = document.getElementById('ethAddress').value;
+    try {
+        const web3 = window.web3;
+
+        // Load your Solidity smart contract ABI and address
+        const contractABI = [
+            // Define your contract ABI here
+        ];
+        const contractAddress = '0xYourContractAddress';
+
+        const contract = new web3.eth.Contract(contractABI, contractAddress);
+
+        // Call a function on the smart contract to retrieve data
+        const contractData = await contract.methods.yourFunction().call({ from: ethAddress });
+
+        // Display the contract data
+        const contractDataDisplay = document.getElementById('contractData');
+        contractDataDisplay.innerHTML = `<h2>Contract Data</h2>`;
+        contractDataDisplay.innerHTML += `<p>Your data: ${contractData}</p>`;
+    } catch (error) {
+        console.error('Error fetching contract data:', error);
+        alert('Error fetching contract data. Please check the Ethereum address and contract configuration.');
+    }
+}
+
+// Connect to Ethereum when the page loads
+window.addEventListener('load', function () {
+    connectToEthereum();
+});
+
+// Attach click event handler to the button
+document.getElementById('getDataButton').addEventListener('click', getContractData);
